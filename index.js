3f1808626b1910827c6119a8fd22142fdf9a95f5
vineet8585 <vineetsharma67727@gmail.com>
2026-08-20 12:10:19 +0530

    uodate

--- index.js
+++ index.js
@@ -1,159 +1,169 @@
 
-console.log("Hello, World!");
-let a=23;
+// console.log("Hello, World!");
+// let a=23;
 
 
-if(a>10){
-    let a=15;
-    console.log("The value of a is greater than 10, so we changed it to: " + a);
-}
-console.log("The final value of a is: " + a);
-function sum(){
-    return 20 + 20;
-};
+// if(a>10){
+//     let a=15;
+//     console.log("The value of a is greater than 10, so we changed it to: " + a);
+// }
+// console.log("The final value of a is: " + a);
+// function sum(){
+//     return 20 + 20;
+// };
 
-function sumofsum(){
-console.log(sum()+sum());
-}
+// function sumofsum(){
+// console.log(sum()+sum());
+// }
 
-sumofsum();
+// sumofsum();
 
-function info(roll, name, college, branch) {
-    return "Roll: " + roll +
-           " Name: " + name +
-           " College: " + college +
-           " Branch: " + branch;
-}
+// function info(roll, name, college, branch) {
+//     return "Roll: " + roll +
+//            " Name: " + name +
+//            " College: " + college +
+//            " Branch: " + branch;
+// }
 
-function myinfo() {
-    console.log(info(
-        202501030011,
-        "Vineet Sharma",
-        "ABES Engineering College",
-        "CSE"
-    ));
-}
+// function myinfo() {
+//     console.log(info(
+//         202501030011,
+//         "Vineet Sharma",
+//         "ABES Engineering College",
+//         "CSE"
+//     ));
+// }
 
-function myfriendinfo() {
-console.log(info(
-    202501030011,
-    "AJAY",
-    "ABES Engineering College",
-    "CSE"
-));
-}
-
-
-
-
-myinfo();
-myfriendinfo();
-
-
-function generateRandomNumber() {
-    return Math.floor(Math.random() * 1000) ;
-
-} 
-const randomNumber = generateRandomNumber();
-console.log("Random Number: " + randomNumber);
-
-function checkEvenOdd(number) {
-if (randomNumber % 2 === 0) {
-    console.log(" even");
-} else {
-    console.log(" odd");
-}
-} 
-
-checkEvenOdd(randomNumber);
+// function myfriendinfo() {
+// console.log(info(
+//     202501030011,
+//     "AJAY",
+//     "ABES Engineering College",
+//     "CSE"
+// ));
+// }
 
 
 
-const sum=(a,b)=>{
-    return a+b;
-}
-console.log(sum(3,3));
 
-(()=>{ console.log("Hello, World!"); })();
+// myinfo();
+// myfriendinfo();
 
 
-function sum(a, b) {
-    return a + b;
-}
+// function generateRandomNumber() {
+//     return Math.floor(Math.random() * 1000) ;
 
-function suminMsg(sum, msg) {
-    const result = sum(10, 20);
-    return msg + result;
-}
+// } 
+// const randomNumber = generateRandomNumber();
+// console.log("Random Number: " + randomNumber);
 
-console.log(suminMsg(sum, "The sum of 10 and 20 is: "));
+// function checkEvenOdd(number) {
+// if (randomNumber % 2 === 0) {
+//     console.log(" even");
+// } else {
+//     console.log(" odd");
+// }
+// } 
+
+// checkEvenOdd(randomNumber);
 
 
-function login(msg,error){
-    if(error){
-        console.log("Error: " + error);
 
-}
-else{
-    console.log("Success: " + msg);
-}
-}
+// const sum=(a,b)=>{
+//     return a+b;
+// }
+// console.log(sum(3,3));
 
-function loginUser(username, password, callback){
+// (()=>{ console.log("Hello, World!"); })();
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function suminMsg(sum, msg) {
+//     const result = sum(10, 20);
+//     return msg + result;
+// }
+
+// console.log(suminMsg(sum, "The sum of 10 and 20 is: "));
+
+
+// function login(msg,error){
+//     if(error){
+//         console.log("Error: " + error);
+
+// }
+// else{
+//     console.log("Success: " + msg);
+// }
+// }
+
+// function loginUser(username, password, callback){
     
-        if(username==="Vineet" && password==="12345"){
-            callback("Login Successful", null);
-        }
-        else{
-            callback(null, "Invalid username or password");
-        }         
-    }
+//         if(username==="Vineet" && password==="12345"){
+//             callback("Login Successful", null);
+//         }
+//         else{
+//             callback(null, "Invalid username or password");
+//         }         
+//     }
 
-    loginUser("Vineet", "12345", login);
+//     loginUser("Vineet", "12345", login);
 
-    setTimeout(()=>(console.log("Hello")),3000);
+//     setTimeout(()=>(console.log("Hello")),3000);
 
-    setTimeout(() => {
-        console.log("one");
+//     setTimeout(() => {
+//         console.log("one");
     
-        setTimeout(() => {
-            console.log("two");
+//         setTimeout(() => {
+//             console.log("two");
     
-            setTimeout(() => {
-                console.log("three");
+//             setTimeout(() => {
+//                 console.log("three");
     
-                setTimeout(() => {
-                    console.log("four");
+//                 setTimeout(() => {
+//                     console.log("four");
     
-                    setTimeout(() => {
-                        console.log("five");
+//                     setTimeout(() => {
+//                         console.log("five");
     
-                        setTimeout(() => {
-                            console.log("six");
+//                         setTimeout(() => {
+//                             console.log("six");
 
-                          setTimeout(()=> {
+//                           setTimeout(()=> {
 
-                            console.log("seven");
+//                             console.log("seven");
 
-                            setTimeout(()=> {
-                               console.log("eight"); 
-                          })
-                        }, 1000);
+//                             setTimeout(()=> {
+//                                console.log("eight"); 
+//                           })
+//                         }, 1000);
     
-                    }, 1000);
+//                     }, 1000);
     
-                }, 1000);
+//                 }, 1000);
     
-            }, 1000);
+//             }, 1000);
     
-        }, 1000);
+//         }, 1000);
     
-    }, 1000);
+//     }, 1000);
 
-}, 1000);
+// }, 1000);
     
 
-console.log("one");
-console.log("two");
-console.log("three");
-console.log("four");
+// console.log("one");
+// console.log("two");
+// console.log("three");
+// console.log("four");
+
+function sumofsqrt(a,b)
+{
+    return Math.sqrt(a)+Math.sqrt(b);
+}
+
+function myname(){
+    return "Vineet Sharma"
+}
+console.log(myname()+" "+ sumofsqrt(4,9));
